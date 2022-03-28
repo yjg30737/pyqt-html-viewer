d0f16c50b143dd07b4e63b6cd0ef1ca3baea44fa
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-28 15:34:10 +0900

    Don't be too specific; change the install-required Qt version from 5.15.1 to 5.15

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description='PyQt html viewer application',
     url='https://github.com/yjg30737/pyqt-html-viewer.git',
     install_requires=[
-        'PyQt5>=5.15.1',
+        'PyQt5>=5.15',
         'pyqtwebengine',
         'pyqt-checkbox-list-widget @ git+https://git@github.com/yjg30737/pyqt-checkbox-list-widget.git@main',
         'pyqt-style-setter @ git+https://git@github.com/yjg30737/pyqt-style-setter.git@main',
