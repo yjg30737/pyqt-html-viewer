7b96ffa2981008b10ab1f536efc0498df085de5c
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-29 13:00:12 +0900

    Add new function (removeSomeFilesFromViewer)

--- pyqt_html_viewer/htmlViewerWidget.py
+++ pyqt_html_viewer/htmlViewerWidget.py
@@ -11,3 +11,21 @@ class HtmlViewerWidget(ViewerWidget):
     def __initUi(self):
         mainWidget = HtmlViewerView()
         self.setView(mainWidget)
+
+    def removeSomeFilesFromViewer(self, filenames_to_remove: list):
+        cur_idx = self.getCurrentIndex()
+        filenames = self.getFilenames()
+        maintain_cur_idx_if_cur_idx_file_still_remain = filenames[cur_idx] in filenames_to_remove
+
+        for filename in filenames_to_remove:
+            filenames.remove(filename)
+
+        if len(filenames) == 0:
+            pass
+        elif len(filenames) > cur_idx:
+            if maintain_cur_idx_if_cur_idx_file_still_remain:
+                self.setCurrentIndex(filenames.index(filenames[cur_idx]))
+            else:
+                self.setCurrentIndex(cur_idx-(len(filenames)-len(filenames_to_remove)))
+        elif len(filenames) <= cur_idx:
+            self.setCurrentIndex(-1)
