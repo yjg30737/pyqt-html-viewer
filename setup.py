c21e957694cffc7f43e05c086cf8a3118abb1147
Jung Gyu Yoon <yjg30737@gmail.com>
2022-03-29 11:41:32 +0900

    Update version (0.3.2 -> 0.4.0)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pyqt-html-viewer',
-    version='0.3.2',
+    version='0.4.0',
     author='Jung Gyu Yoon',
     author_email='yjg30737@gmail.com',
     license='MIT',
