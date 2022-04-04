ed3ee5220e67571943ff66dcdf5d77159e3bd960
Jung Gyu Yoon <yjg30737@gmail.com>
2022-04-04 12:57:56 +0900

    Add missing module to __init__.py

--- pyqt_html_viewer/__init__.py
+++ pyqt_html_viewer/__init__.py
@@ -1,5 +1,6 @@
 from .htmlViewer import HtmlViewer
 from .fileWidget import FileWidget
 from .htmlViewerWidget import HtmlViewerWidget
-from .sourceWidget import SourceWidget
+from .htmlViewerView import HtmlViewerView
 from .htmlViewerApp import HtmlViewerApp
+from .sourceWidget import SourceWidget
